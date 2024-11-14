1fb531d66da907cb05b11b9d938e118b6b1b9ec4
Tatiana Campos <71901494+tatiana00017@users.noreply.github.com>
2024-11-13 23:16:53 -0500

    Add files via upload

---
+++ CursoJava/src/hola_mundo/HolaMundo.java
@@ -0,0 +1,9 @@
+package hola_mundo;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Saludos");
+        int a =5;
+    }
+}
