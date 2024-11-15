ebe9e932f78421b0cfdfea035a88be778c1ad4a1
tatiana00017 <tatis00017@gmail.com>
2024-11-14 21:47:49 -0500

    Añadir variables

---
+++ CursoJava/src/variables/Variables.java
@@ -0,0 +1,20 @@
+package variables;
+
+public class Variables {
+    public static void main(String[] args) {
+        // Variables
+        int edad = 24;
+        double precio= 300.30;
+        boolean disponible = true;
+        char genero;
+        genero= 'M';
+
+        System.out.println(edad);
+        edad=23;
+        System.out.println(edad);
+
+        System.out.println(precio);
+        System.out.println(disponible);
+        System.out.println(genero);
+    }
+}
