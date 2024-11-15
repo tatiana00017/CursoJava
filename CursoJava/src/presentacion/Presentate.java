99d92e147f6a26c8a19cbd408ca74f165fcecc13
tatiana00017 <tatis00017@gmail.com>
2024-11-14 21:29:54 -0500

    añadir presentacion

---
+++ CursoJava/src/presentacion/Presentate.java
@@ -0,0 +1,7 @@
+package presentacion;
+
+public class Presentate {
+    public static void main(String[] args) {
+        System.out.println("Hola mi nombre es Leidy Tatiana Campos Roncancio, tengo 24 años y soy de colombia");
+    }
+}
