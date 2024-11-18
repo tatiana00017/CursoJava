a172592c2081242e172888fcbca68e95fc28529d
tatiana00017 <tatis00017@gmail.com>
2024-11-18 14:32:55 -0500

    add Detalle Libro

---
+++ CursoJava/src/detallelibro/DetalleLibro.java
@@ -0,0 +1,14 @@
+package detallelibro;
+
+public class DetalleLibro {
+    public static void main(String[] args) {
+        String tituloLibro="El señor de los anillos";
+        int anioPublicarion= 1954;
+        boolean libroDisponible=true;
+        double precioLibro=500.50;
+        System.out.println(tituloLibro);
+        System.out.println(anioPublicarion);
+        System.out.println(libroDisponible);
+        System.out.println(precioLibro);
+    }
+}
