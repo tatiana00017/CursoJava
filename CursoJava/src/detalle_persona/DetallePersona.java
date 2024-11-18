13d1772348bd4efb48996f39436851530ea59747
tatiana00017 <tatis00017@gmail.com>
2024-11-18 14:45:50 -0500

    add DetallePersona

---
+++ CursoJava/src/detalle_persona/DetallePersona.java
@@ -0,0 +1,13 @@
+package detalle_persona;
+
+public class DetallePersona {
+    public static void main(String[] args) {
+        String nombreCompleto= "Leidy Tatiana Campos Roncancio";
+        int edad=24;
+        double altura=1.50;
+        String paisOrigen="Colombia";
+        char estadoCivil='S';
+        System.out.println("Mi nombre es "+nombreCompleto+" tengo "+edad+" años y mido "+altura+" mts soy de "+paisOrigen+" y mi estado civil es "+estadoCivil);
+    }
+
+}
